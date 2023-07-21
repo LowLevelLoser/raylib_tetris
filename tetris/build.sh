2ea21b831afae603de6e8a359f834937384e43c2
eddi <108631435+LowLevelLoser@users.noreply.github.com>
2023-07-21 13:23:48 -0500

    Add files via upload

---
+++ tetris/build.sh
@@ -0,0 +1,1 @@
+cc -o Tetris main.c rendering.c logic.c -s -Wall -lraylib -lm -lpthread -ldl -lrt -lX11
---
+++ tetris/rendering.h
@@ -0,0 +1,8 @@
+#ifndef  RENDERING_H_
+#define RENDERING_H_
+
+#include "game.h"
+
+void RenderGame(const game_t *game);
+
+#endif // !DEBUG
