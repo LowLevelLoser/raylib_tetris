5c51c07614b2aac34bf45ea1e03a7c06e5b06962
eddi <108631435+LowLevelLoser@users.noreply.github.com>
2023-09-08 16:36:32 -0500

    Delete tetris directory

--- tetris/logic.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef  LOGIC_H_
-#define LOGIC_H_
-
-#include "game.h"
-
-void RunGame(game_t *game);
-
-#endif // !DEBUG
--- tetris/rendering.h
+++
@@ -1,8 +0,0 @@
-#ifndef  RENDERING_H_
-#define RENDERING_H_
-
-#include "game.h"
-
-void RenderGame(const game_t *game);
-
-#endif // !DEBUG
